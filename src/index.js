fc8f4aa198c1204986a07c5d0c0e869ebd340448
zezinhacker <pedroaugustobrusque06@gmail.com>
2023-03-13 14:43:50 -0300

    ai

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './CSS/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './REPORT/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
